fd57df079c3a38f24a0e8c741741eb9e3daf8b90
Juanchambi321 <126572521+Juanchambi321@users.noreply.github.com>
2024-08-12 01:40:39 -0300

    Add files via upload

--- PlayTacks/login/inicio.php
+++ PlayTacks/login/inicio.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>pagina_principal2</title>
-    <link rel="stylesheet" href="../css/pagina_principal2.css">
+    <link rel="stylesheet" href="http://localhost/PlayTacks/PlayTacks/css/pagina_principal2.css">
     <script src="../js/pagina_principal2.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
